4b972ac0fb3e78b5cfc71366487dc1f73d2335fc
Raj Sharma <xrehpicx@gmail.com>
2021-03-31 21:01:47 +0530

    lot-of-things

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 process.env.NODE_ENV !== 'production' && require("dotenv").config();
 const Bot = require('./lib/Bot');
 
-const rbot = Bot('rbot');
-// const rbot = Bot('theia_bot',null,'localhost',60506);
+// const rbot = Bot('rbot');
+const rbot = Bot('theia_bot',null,'192.168.29.172',61638);
 
