30ceb9c96cf67480af3eec10057e912844c004af
Raj Sharma <xrehpicx@gmail.com>
2021-04-15 19:06:55 +0530

    rbot-afk

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ process.env.NODE_ENV !== 'production' && require("dotenv").config();
 const Bot = require('./lib/Bot');
 
 // const rbot = Bot('rbot');
-const rbot = Bot('theia_bot',null,'192.168.29.172',61638);
+// const rbot = Bot('theia_bot',null,'192.168.29.172',61638);
+const rbot = Bot('rbot');
+// const rbot = Bot('theia_bot');
 
