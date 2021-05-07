77149854444f390a200dfbf44259d457bdd585b5
Raj Sharma <xrehpicx@gmail.com>
2021-05-07 13:42:34 +0530

    idontevenremember

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 process.env.NODE_ENV !== 'production' && require("dotenv").config();
 const Bot = require('./lib/Bot');
 
-// const rbot = Bot('rbot');
-// const rbot = Bot('theia_bot',null,'192.168.29.172',61638);
-const rbot = Bot('rbot');
-const tbot = Bot('theia_bot');
 
+module.exports = Bot
