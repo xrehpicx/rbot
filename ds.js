cf6dd5efe7da9d4c4ae0b145184973e0c9ffd722
Raj Sharma <xrehpicx@gmail.com>
2021-05-14 01:07:23 +0530

    idk

---
+++ ds.js
@@ -0,0 +1,14 @@
+process.env.NODE_ENV !== 'production' && require("dotenv").config();
+const axios = require('axios');
+const Bot = require('./lib/Bot');
+
+const tbot = new Bot({ name: 'drownedslayer', host: '135.125.16.188', port: 25575, admin: 'nrohan09' });
+// const tbot = new Bot({ name: 'tbot', host: '135.125.16.188', port: 25575, cli: false });
+process.on('exit', onend);
+// process.on('SIGINT', onend);
+
+function onend(reason) {
+    axios.post('https://evve.herokuapp.com/api/notify', { title: 'rbot left', description: reason || 'rbot left the world' }).catch(() => { console.log('failed to notify discord') })
+}
+
+// axios.post('https://evve.herokuapp.com/api/notify', { title: 'rbot', description: 'rbot joined the world' }).catch(() => { console.log('failed to notify discord') });
